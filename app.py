78f0a0c3c2ae0238d05432116369b9a8c7f41a8f
rachelann1028 <rachelanncastaneda@gmail.com>
2025-07-30 09:16:24 -0600

    chart

--- app.py
+++ app.py
@@ -28,7 +28,7 @@ with st.expander("Data"):
 
 
 with st.expander("Data Visualization"):
-    pass
+    st.scatter_chart(data=df, x='bill_lenght_mm', y='body_mass_g', color = 'species')
 
 with st.expander("Input Data"):
     pass
