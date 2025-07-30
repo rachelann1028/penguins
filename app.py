d7ac383ab216d1b4020e98b5c4b6b301a70f0573
rachelann1028 <rachelanncastaneda@gmail.com>
2025-07-30 08:37:08 -0600

    Create app.py

---
+++ app.py
@@ -0,0 +1,22 @@
+import pandas as pd
+import streamlit as st
+import numpy as np
+from sklearn.ensemble import RandomForestClassifier
+
+st.title("Penguine species prediction")
+st.info("This is end-to-end Machine Learning app")
+
+with st.expander("Data"):
+    pass
+
+with st.expander("Data Visualization"):
+    pass
+
+with st.expander("Input Data"):
+    pass
+
+with st.expander("Data preparation"):
+    pass
+
+with st.sidebar:
+    pass
